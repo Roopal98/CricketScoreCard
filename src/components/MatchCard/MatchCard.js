99cb144416ce5628c636245cb763f5ef3c66765b
Roopal98 <roopal.dumka@gmail.com>
2022-10-26 01:09:35 +0530

    added-pics

--- src/components/MatchCard/MatchCard.js
+++ src/components/MatchCard/MatchCard.js
@@ -13,23 +13,26 @@ const MatchCard = () => {
         <Card className="bg-dark text-white">
     <Card.Img src={background} alt="Card image" className='image' />
     <Card.ImgOverlay>
-      <Container className='d-flex justify-content-center align-items-center flex-column match-card'>
+      <Container className='match-card'>
         {/* first row */}
-          <Row className='match-card-info'>ICC Men's T20 World Cup- Australia vs Sri Lanka, 19th Match</Row>
+          <Row className='match-card-info d-flex align-items-center flex-column'>ICC Men's T20 World Cup- Australia vs Sri Lanka, 19th Match</Row>
           {/* second row */}
           <Row className='match-card-score'>
-            <Col cassName='d-flex justify-content-flex-start' lg={4}>
-              Austrilia
-              Sri Lanka
-            </Col>
-            <Col cassName='d-flex justify-content-flex-end match-card-teams' lg={4}>
-              156/3
-              157/6 
-            </Col>
+            <Col lg={4}><span><img
+              src="https://images.entitysport.com/assets/uploads/2020/12/India.png"
+              alt=""
+            /></span>IND</Col>
+            <Col lg={4}> 212/2</Col>
           </Row>
-          {/* 3rd row */}
-          <Row className='match-card-footer'>
-            Tue, 25 Oct, Austrilia won by 7 wickets. Man of the match: Marcus Stoinis
+          <Row className='match-card-score'>
+            <Col lg={4}><span><img
+              src="https://images.entitysport.com/assets/uploads/2020/12/Afghanistan.png"
+              alt=""
+            /></span> AFG</Col>
+            <Col lg={4}> 111/2</Col>
+          </Row>
+          <Row className='match-card-footer d-flex align-items-center flex-column'>
+            Tue, 25 Oct, India won by 100 runs. Man of the match: Virat Kohli
           </Row>
         </Container>
     </Card.ImgOverlay>
